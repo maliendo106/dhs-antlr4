28767fec3cd9dcec06247dfe1f726feb28168fc9
Mateo Fransisco Pagnucco <mateopagnucco@gmail.com>
2021-11-11 21:16:37 -0300

    mateo esta despierto... O.O

--- src/main/python/App.py
+++ src/main/python/App.py
@@ -5,20 +5,22 @@ from compiladorParser import compiladorParser
 #from main.python.miListener import miListener
 #from fechasLexer import fechasLexer
 #from fechasParser import fechasParser
-from miListener import miListener # agregado
+from miListener import miListener  # agregado
+
 
 def main(argv):
-    archivo = "../../entrada.txt" #entrada.txt
-    if len(argv) > 1 :
+    archivo = "../../entrada.txt"  # entrada.txt
+    if len(argv) > 1:
         archivo = argv[1]
     input = FileStream(archivo)
     lexer = compiladorLexer(input)
     stream = CommonTokenStream(lexer)
-    parser = compiladorParser(stream) 
+    parser = compiladorParser(stream)
     escucha = miListener()          # agregado
     parser.addParseListener(escucha)    # agregado
     tree = parser.programa()
     print(tree.toStringTree(recog=parser))
+
 
 if __name__ == '__main__':
     main(sys.argv)
